e28f97699e485bbf3e15e0172e53489c269c6d36
poloo5582 <poloo5582@yahoo.com.tw>
2015-06-26 16:46:12 +0800

    local

--- LocalExtension/popup.js
+++ LocalExtension/popup.js
@@ -15,26 +15,47 @@ document.addEventListener('DOMContentLoaded', function() {
                     var url = historyItems[i].url;
                     console.log(url);
                     if(url.match(/facebook/)){
-                        return;
+                        continue;
                     }
                     else if(url.match(/http:/)||url.match(/https:/)){
-                    //url = "http://www.google.com.tw"
-                        $.ajax({
-                            url:url,
-                            type: 'GET',
-                            success:function(msg){
-                                console.log('done '+url);
-                                msg = msg.replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '');
-                                msg = msg.replace(/<style[^>]+?\/>|<style(.|\s)*?\/style>/gi, '');
-                                msg = msg.replace(/<(.|\s)*?>/g, '');
-                                console.log(msg);
-                            }
-                        });
+                        getUrlContent(historyItems[i].id,url);
                     }
 
                 }
             }
         );
     };
+    function getUrlContent(id,url){
+        $.ajax({
+            url:url,
+            type: 'GET',
+            success:function(msg){
+                msg = html2text(msg);
+                chrome.storage.sync.set({id:id,data:msg},function(){
+                    console.log('save '+url+' in '+id);
+                });
+            }
+        });
+    }
+    function html2text(html) {
+        html = html.replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/ig, '');
+        html = html.replace(/<noscript[^>]+?\/>|<noscript(.|\s)*?\/noscript>/ig, '');
+        html = html.replace(/<style[^>]+?\/>|<style(.|\s)*?\/style>/ig, '');
+        html = html.replace(/<.*?>/ig, '');
+        html = html.replace(/\s+/ig, ' ');
+        html = html.replace(/&(quot|#34);/ig, '');
+        html = html.replace(/&(amp|#38);/ig, '');
+        html = html.replace(/&(lt|#60);/ig, '');
+        html = html.replace(/&(gt|#62);/ig, '');
+        html = html.replace(/&(nbsp|#160);/ig, '');
+        html = html.replace(/&(iexcl|#161);/ig, '');
+        html = html.replace(/&(cent|#162);/ig, '');
+        html = html.replace(/&(pound|#163);/ig, '');
+        html = html.replace(/&(copy|#169);/ig, '');
+        html = html.replace(/&(reg|#174);/ig, '');
+        html = html.replace(/&#(d+);/, '');
+
+        return html;
+    }
     getRecentHistory(10);
 });
